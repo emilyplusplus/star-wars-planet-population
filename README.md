c6ab380ba2285a301fa2847acbebc7f5e817a8b9
Emily Carlsen <emilytcyt@gmail.com>
2018-08-04 02:34:14 -0400

    Add more comments describing code and add global install/todo to readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A Node.js command line application that uses a publicly accessible SWAPI (Star Wars API, https://swapi.co/) to get the people that are from a given planet in the Star Wars universe.
 
 
-#### Install/Setup Instructions (How To Use) 
+### Developer Install/Setup Instructions (How To Use) 
 To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line: 
 
 ```bash
@@ -15,3 +15,24 @@ npm install
 # Run the app and pass in an argument with planet name
 node main.js -p Coruscant
 ```
+
+### Global Install (Alternative Install Option)
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line: 
+
+```bash
+# Clone this repository
+git clone https://github.com/emilyplusplus/star-wars-planet-population
+# Go into the repository
+cd star-wars-planet-population
+# Install dependencies
+npm install
+# Install globally (you may need sudo depending on permissions on your computer)
+npm i -g ./
+# Run the app and pass in an argument with planet name
+# **** This can now be done from anywhere on your computer regardless of pwd ****
+swpp -p Coruscant
+```
+
+### To Do (Future Ideas)
+1. Add local cache with some expiry time (i.e. 24 hrs) so we don't have to hit API everytime to speed up user results and minimize API/data usage
+2. Add more arguments/make more data accessible via CLI
--- main.js
+++ main.js
@@ -15,10 +15,10 @@ if(!program.planet) {
 
 console.log('Getting data for the planet ' + program.planet + '...');
 
-//Step 1
+//Step 1 - Planets and people will be stored as arrays of objects
 let planets = people = []
 
-//Step 2
+//Step 2 - Fire two async functions (see SWAPI lib file) to get data and store it in main thread
 let planetsPromise = swapi.getPagedResource('/planets').then(data => {
     planets = data
 })
@@ -29,13 +29,13 @@ let peoplePromise = swapi.getPagedResource('/people').then(data => {
 
 Promise.all([planetsPromise, peoplePromise]).then(function(values) {
 
-    //Step 3
+    //Step 3 - When data is avialable from Step 2, print meaningful data (number and names of planets/people) in a readable manner
     console.log('\nTotal indexed planets found: ' + planets.length)
     console.log('Planet names: ' + planets.map( planet => { return planet.name }).join(', ') + '\n')
     console.log('Total indexed people found: ' + people.length)
     console.log('People names: ' + people.map( person => { return person.name }).join(', ') + '\n')
 
-    //Step 4
+    //Step 4 - Createa a population object to hold key/value pairs containing names of native ppl on each planet and populate usuing js Array filter, map, and join
     let population = {}
 
     planets.forEach((planet) => {
@@ -48,8 +48,8 @@ Promise.all([planetsPromise, peoplePromise]).then(function(values) {
         population[planet.name] = names
     })
 
-    //Step 5
-    if(population[program.planet].length == 0) {
+    //Step 5 - Print names of native people for chosen planet, or display appropriate message if no-one is found or planet doesn't exist
+    if(!population[program.planet] || population[program.planet].length == 0) {
         console.error('No native people found :(')
     } else {
         console.log('Native people: ' + population[program.planet])
