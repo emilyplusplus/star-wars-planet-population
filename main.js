85c7770df24338e44881cec6b7ac80e44d225165
Emily Carlsen <emilytcyt@gmail.com>
2018-08-04 01:03:15 -0400

    Add step marker comments

--- main.js
+++ main.js
@@ -15,8 +15,10 @@ if(!program.planet) {
 
 console.log('Getting data for the planet ' + program.planet + '...');
 
+//Step 1
 let planets = people = []
 
+//Step 2
 let planetsPromise = swapi.getPagedResource('/planets').then(data => {
     planets = data
 })
@@ -25,7 +27,10 @@ let peoplePromise = swapi.getPagedResource('/people').then(data => {
     people = data
 })
 
+//Step 3
 Promise.all([planetsPromise, peoplePromise]).then(function(values) {
     console.log('Total indexed planets found: ' + planets.length)
     console.log('Total indexed people found: ' + people.length)
 })
+
+//Step 4
